a283b1da94f365be0e44d7413d06622ac4661539
Ilya <monah58rus@rambler.ru>
2023-09-25 17:11:56 +0300

    add map

--- english_site/landing/static/js/change_map.js
+++ english_site/landing/static/js/change_map.js
@@ -1,32 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
     function resizeMap() {
-        var mapElement = document.getElementById('map');
+        var mapElement = document.getElementById("map");
         var windowWidth = window.innerWidth;
 
         if (windowWidth <= 350) {
-            // Ширина экрана меньше или равна 545
-            mapElement.className = 'smallMap_350';
-        }else if (windowWidth <= 545) {
-            // Ширина экрана меньше или равна 545
-            mapElement.className = 'smallMap_540';
-        } else if (mapElement.offsetWidth <= 767) {
-            // Ширина карты меньше или равна 767
-            mapElement.className = 'smallMap_600';
+          mapElement.classList.remove("smallMap_540");
+          mapElement.classList.remove("smallMap_600");
+          mapElement.classList.add("smallMap_350");
+        } else if (windowWidth <= 545) {
+          mapElement.classList.remove("smallMap_350");
+          mapElement.classList.remove("smallMap_600");
+          mapElement.classList.add("smallMap_540");
+        } else if (windowWidth <= 767) {
+          mapElement.classList.remove("smallMap_350");
+          mapElement.classList.remove("smallMap_540");
+          mapElement.classList.add("smallMap_600");
         } else {
-            // Все остальные случаи
-            mapElement.className = '';
+          mapElement.className = "map"; // Восстановление класса по умолчанию
         }
+      }
 
-        // Обновление карты
-        // Замените 'your-map-object' на объект карты, который у вас есть
-        if (typeof yourMapObject !== 'undefined') {
-            yourMapObject.container.fitToViewport();
-        }
-    }
+      // Слушаем событие изменения размера окна
+      window.addEventListener("resize", resizeMap);
 
-    // Слушаем событие изменения размера окна
-    window.addEventListener('resize', resizeMap);
-
-    // Вызываем функцию при загрузке страницы
-    window.addEventListener('load', resizeMap);
+      // Вызываем функцию при загрузке страницы
+      window.addEventListener("load", resizeMap);
 });
