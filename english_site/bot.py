80e4664dbdd96bb9e4046381ef04766fe1085410
Ilya <monah58rus@rambler.ru>
2023-09-26 11:32:21 +0300

    add tele bot

--- english_site/bot.py
+++ english_site/bot.py
@@ -7,7 +7,7 @@ setup()
 
 import telebot
 from django.core.files import File
-from landing.models import Book  
+from landing.models import Book, Image
 from io import BytesIO
 
 TOKEN = '6415362149:AAHokYJZXCwMTLNUAyr-rxqOJIdSu4OM6p0'
@@ -23,16 +23,12 @@ chat_file_types = {}
 def start(message):
     chat_id = message.chat.id
 
-    # Проверяем, если пользователь находится в процессе добавления файлов, то выходим из него
-    if chat_id in chat_file_types:
-        del chat_file_types[chat_id]
-        bot.send_message(chat_id, "Вы вышли из процесса добавления файлов. Пожалуйста, начните снова с команды /start.")
-        return
-
     # Просим пользователя отправить наименование книги
     bot.send_message(chat_id, "Пожалуйста, отправьте наименование книги.")
 
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler(func=lambda message: True 
+                     and not message.text.lower() == '/exit' 
+                     and not message.text.lower() == '/start')
 def receive_title(message):
     chat_id = message.chat.id
 
@@ -52,43 +48,13 @@ def receive_title(message):
     # Устанавливаем ожидание файла книги
     chat_file_types[chat_id] = "book"
 
-@bot.message_handler(commands=['add'])
-def add_files(message):
-    chat_id = message.chat.id
-    bot.send_message(chat_id, "Пожалуйста, отправьте ID книги, к которой вы хотите добавить файлы.")
-    chat_file_types[chat_id] = "add"
-
-@bot.message_handler(func=lambda message: True)
-def add_files_by_id(message):
-    chat_id = message.chat.id
-
-    if chat_id in chat_file_types and chat_file_types[chat_id] == "add":
-        book_id = message.text
-
-        try:
-            book_id = int(book_id)
-        except ValueError:
-            bot.send_message(chat_id, "ID книги должен быть числом. Пожалуйста, попробуйте еще раз.")
-            return
-
-        book_entry = Book.objects.filter(id=book_id).first()
-
-        if not book_entry:
-            bot.send_message(chat_id, "Книга с указанным ID не найдена. Пожалуйста, попробуйте еще раз.")
-            return
-
-        chat_book_ids[chat_id] = book_id
-        bot.send_message(chat_id, "Теперь отправьте файлы для добавления к этой книге.")
-        return
-
-    bot.send_message(chat_id, 'Неожиданное сообщение. Пожалуйста, начните снова с команды /start или /add.')
-
 @bot.message_handler(content_types=['document', 'photo'])
 def receive_files(message):
+    print(chat_book_ids, chat_file_types)
     chat_id = message.chat.id
 
     if chat_id not in chat_file_types:
-        bot.send_message(chat_id, 'Неожиданное сообщение. Пожалуйста, начните снова с команды /start или /add.')
+        bot.send_message(chat_id, 'Неожиданное сообщение. Пожалуйста, начните снова с команды /start.')
         return
 
     if message.document:
@@ -98,7 +64,8 @@ def receive_files(message):
     elif message.photo:
         file_info = bot.get_file(message.photo[-1].file_id)
         file = bot.download_file(file_info.file_path)
-        name = "image.jpg"  # Имя по умолчанию для фотографии
+        print(file_info)
+        name = 'image.jpg' # Имя по умолчанию для фотографии
 
     # Получаем ID книги из chat_book_ids
     book_id = chat_book_ids.get(chat_id)
@@ -110,23 +77,41 @@ def receive_files(message):
             if chat_file_types[chat_id] == "book":
                 book_entry.file.save(name, File(file_buffer))
                 # Просим пользователя отправить изображение
-                bot.send_message(chat_id, "Теперь отправьте изображение для этой книги.")
+                bot.send_message(chat_id, "Теперь отправьте изображение(или несколько) для этой книги.")
                 # Устанавливаем ожидание изображения
                 chat_file_types[chat_id] = "image"
             elif chat_file_types[chat_id] == "image":
-                book_entry.image.save(name, File(file_buffer))
-                # Удаляем соответствие между ID чата и ID книги
-                del chat_book_ids[chat_id]
-                del chat_file_types[chat_id]
-                # Отправляем сообщение о сохранении файла и ID книги
-                bot.send_message(chat_id, f'Файл "{name}" сохранен в базе данных. ID книги: {book_id}')
+                # Сохраняем изображение в модель Image
+                image = Image()
+                image.file.save(name, File(file_buffer))
+                image.save()
 
-        # Удаляем чат из словарей
-        if chat_id in chat_file_types:
-            del chat_file_types[chat_id]
+                # Добавляем изображение к списку изображений книги
+                book_entry.image.add(image)
+                # Отправляем сообщение о сохранении файла и ID книги
+
+                bot.send_message(chat_id, f'Файл "{name}" сохранен в базе данных. ID книги: {book_id}'
+                                 f'\nВы можете добавить файлы к этой книге. Напишите /exit если хотите выйти из этой книги.')
         return
 
-    bot.send_message(chat_id, 'Не найдена связь с книгой. Пожалуйста, начните снова с команды /start или /add.')
+    bot.send_message(chat_id, 'Не найдена связь с книгой. Пожалуйста, начните снова с команды /start.')
 
 
-bot.polling()
+@bot.message_handler(commands=['exit'])
+def start(message):
+
+    chat_id = message.chat.id
+
+    # Проверяем наличие ключа чата в словаре chat_book_ids
+    if chat_id in chat_book_ids:
+        # Удаляем соответствие между ID чата и ID книги
+        del chat_book_ids[chat_id]
+
+    # Проверяем наличие ключа чата в словаре chat_file_types
+    if chat_id in chat_file_types:
+        # Удаляем соответствие между ID чата и типом файла
+        del chat_file_types[chat_id]
+
+    bot.send_message(chat_id, "Вы можете добавить новую книгу. Напишите наименование книги.")
+
+bot.polling(none_stop=True)
--- english_site/landing/models.py
+++ english_site/landing/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    image = models.FileField(upload_to='book_images/')
+    image = models.ManyToManyField('Image', blank=True)
     file = models.FileField(upload_to='book_files/')
-    user = models.CharField(max_length=100)  # Максимальная длина может быть на ваше усмотрение
 
     def __str__(self):
         return self.title
+    
+class Image(models.Model):
+    file = models.FileField(upload_to='book_images/')
